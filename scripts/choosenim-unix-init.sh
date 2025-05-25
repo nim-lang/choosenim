39dff3164d4413c4954661b54d9569c14bb47899
David Dorfman <daviddorfman42@gmail.com>
2025-05-25 20:17:22 +0300

    Use fish_add_path instead of modifying fish_user_paths directly

--- scripts/choosenim-unix-init.sh
+++ scripts/choosenim-unix-init.sh
@@ -90,7 +90,7 @@ install() {
     case "${SHELL:=sh}" in
       *fish*)
       say "Running fish shell?"
-      say "set -ga fish_user_paths $nimbleBinDir"
+      say "fish_add_path $nimbleBinDir"
     ;;
     esac
   fi
