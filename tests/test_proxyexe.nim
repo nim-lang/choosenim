95eeecab1b66c9bb573eeb415432b35237981289
Christopher Arndt <chris@chrisarndt.de>
2025-07-07 22:21:49 +0200

    [tests] fix: improve proxyexe test for getSelectedPath
    Test failed if version/channel of Nim building test doesn't match current
    version/channel set for choosenim
    
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- tests/test_proxyexe.nim
+++ tests/test_proxyexe.nim
@@ -4,29 +4,44 @@ import choosenimpkg/cliparams
 import choosenimpkg/proxyexe
 
 
-proc toExe(filename: string): string =
-  when defined(windows):
-    filename & ".exe"
-  else:
-    filename
+var
+  testDir = currentSourcePath.parentDir
+  nimbleDir = testDir / "nimbleDir"
+  choosenimDir = testDir / "choosenimDir"
+  currentFile = choosenimDir / "current"
+  selectedVersion = choosenimDir / "toolchains" / "nim-" & $NimVersion
 
 
 suite "proxyexe":
+  setup:
+    removeDir(nimbleDir)
+    createDir(nimbleDir)
+    removeDir(choosenimDir)
+    createDir(choosenimDir)
+
+  teardown:
+    removeDir(nimbleDir)
+    removeDir(choosenimDir)
+
   test "can create new CliParams with proxyExeMode = true":
     var params = newCliParams(proxyExeMode = true)
-    check(params.commands.len == 0)
-    check(params.nimbleOptions.startDir == getCurrentDir())
+    check params.commands.len == 0
+    check params.nimbleOptions.startDir == getCurrentDir()
 
-  test "can getSelectedPath and returned path matches Nim version":
+  test "can getSelectedPath and returned path matches":
     var params: CliParams
+    writeFile(currentFile, selectedVersion)
     parseCliParams(params, proxyExeMode = true)
+    params.choosenimDir = choosenimDir
+    check params.getCurrentFile() == currentFile
     let path = getSelectedPath(params)
-    check(path == readFile(params.getCurrentFile()))
-    check(path.lastPathPart() == "nim-" & NimVersion)
+    check path == readFile(params.getCurrentFile())
 
   test "can get executable name and bin path":
     var params: CliParams
+    writeFile(currentFile, selectedVersion)
     parseCliParams(params, proxyExeMode = true)
+    params.choosenimDir = choosenimDir
     let res = getExePath(params)
-    check(res.name == toExe("test_proxyexe"))
-    check(res.path == getSelectedPath(params) / params.getBinDir() / toExe("test_proxyexe"))
+    check res.name == "test_proxyexe"
+    check res.path == getSelectedPath(params) / "bin" / "test_proxyexe".changeFileExt(ExeExt)
